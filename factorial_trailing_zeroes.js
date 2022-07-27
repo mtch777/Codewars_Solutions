e8772e274f6592cebac131275a97f09f87b59196
Mitch Williams <mitch.williams.career@gmail.com>
2022-07-27 08:46:06 -0600

    Write a program that will calculate the number of trailing zeros in a factorial of a given number.
    N! = 1 * 2 * 3 *  ... * N

---
+++ factorial_trailing_zeroes.js
@@ -0,0 +1,14 @@
+function zeros (n) {
+    if(n < 0) //Negative Number Edge Case
+        return -1;
+       
+      // Initialize result
+      let count = 0;
+   
+      // Keep dividing n by powers of
+      // 5 and update count
+      for (let i = 5; Math.floor(n / i) >= 1; i *= 5)
+          count += Math.floor(n / i);
+   
+      return count;
+  }
